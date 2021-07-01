23b70872eef1148abfba6245ba8e845833bcb409
unknown <b07902042@ntu.edu.tw>
2021-07-01 23:47:22 +0800

    first

--- runServer.sh
+++ runServer.sh
@@ -1,5 +1,5 @@
 
 rm -r out/
-javac -cp . -sourcepath ./src/hearthclone -d ./out  ./src/hearthclone/*.java ./src/hearthclone/*/*.java ./src/hearthclone/*/*/*.java
+javac -cp . -sourcepath ./src/hearthclone -d ./out  ./src/*.java ./src/hearthclone/*/*.java ./src/hearthclone/*/*/*.java
 java -cp out/ ServerMain
 
