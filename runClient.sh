17edb65035e70ed6fb2174a04e625a35b01fae90
unknown <b07902042@ntu.edu.tw>
2021-07-01 23:55:34 +0800

    final

--- runClient.sh
+++ runClient.sh
@@ -1,4 +1,4 @@
 
 rm -r out/
-javac -cp . -sourcepath ./src/hearthclone -d ./out  ./src/hearthclone/*.java ./src/hearthclone/*/*.java ./src/hearthclone/*/*/*.java
+javac -cp . -sourcepath ./src/hearthclone -d ./out  ./src/*.java ./src/hearthclone/*/*.java ./src/hearthclone/*/*/*.java
 java -cp out/ ClientMain $1 $2
